d88c7be8d28a9fcd1f9a197e5ecbe20bbeb8234d
saurav526 <sauravtiwari2712005@gmail.com>
2026-01-15 19:43:25 +0530

    using pandas and matplotlib together

--- DATA INSPECTION & INFORMATION/isnull_memoeyuse.py
+++ DATA INSPECTION & INFORMATION/isnull_memoeyuse.py
@@ -1,15 +1,22 @@
-import pandas as pd
+# import pandas as pd
 
-data = {
-    "Name": ["Amit", "Neha", "Rahul", "Priya", "Suresh"],
-    "Age": [21, 22, 23, None, 24],
-    "Marks": [85, 90, 78, 88, None],
-    "City": ["Mumbai", "Delhi", "Pune", "Mumbai", "Delhi"]
-}
+# data = {
+#     "Name": ["Amit", "Neha", "Rahul", "Priya", "Suresh"],
+#     "Age": [21, 22, 23, None, 24],
+#     "Marks": [85, 90, 78, 88, None],
+#     "City": ["Mumbai", "Delhi", "Pune", "Mumbai", "Delhi"]
+# }
 
-df = pd.DataFrame(data)
-print(df) 
+# df = pd.DataFrame(data)
+# print(df) 
 
-print(df.isnull())
-print("\nMemory Usage of DataFrame:")
-print(df.memory_usage(deep=True))
+# print(df.isnull())
+# print("\nMemory Usage of DataFrame:")
+# print(df.memory_usage(deep=True))
+
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+
+plt.plot(x, y , marker='o' ,color='b',  linestyle='--', linewidth=2, markersize=8)
+plt.show()
